819d2e954a036633916f852a03e0adeaa80e8730
HanBnrd <johann.benerradi@gmail.com>
2018-02-17 19:47:51 +0100

    Update end

--- src/allDeadBackward.py
+++ src/allDeadBackward.py
@@ -14,16 +14,16 @@ width=input("Largeur de la grille : ")
 print("*****")
 start_time = time.time()
 r=rl.Rules(int(height),int(width)) # création d'un objet Rules
-isEnd=False
+end=False
 result=[r.allDead()] # liste des états menant à tout mort initialisée avec tout mort
-while not isEnd:
-    isEnd=True
+while not end:
+    end=True
     for state in r.getAll():
         if any((state == x).all() for x in result):
             continue
         elif any((r.next(state) == x).all() for x in result):
             result.append(state)
-            isEnd=False
+            end=False
 print("Proportion d'états menant à tout mort :")
 print(len(result),"/",2**(int(height)*int(width)))
 print("--- %s seconds ---" % (time.time() - start_time))
